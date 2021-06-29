580b723367057c6fe018c8484824d49d1504cd9b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-06-30 02:54:31 +0430

    added boilerplates

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils 
 mp_holistic = mp.solutions.holistic
+
+cam = cv2.VideoCapture(1) 
+
+while cam.isOpened():
+    _, frame = cam.read()
+    cv2.imshow("Camera feed", frame)
+
+    if cv2.WaitKey(0) & 0xff == ord('q'):
+        break 
+
+cam.release()
+cv2.destroyAllWindows()
