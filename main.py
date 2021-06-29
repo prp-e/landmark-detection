79d718ea764526b825e7d2d4af2d23c755060015
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-06-30 03:01:08 +0430

    Worked on landmark detection

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ mp_holistic = mp.solutions.holistic
 
 cam = cv2.VideoCapture(1) 
 
-while True:
-    _, frame = cam.read()
-    cv2.imshow("Camera feed", frame)
-
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break 
+with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as holistic:
+    while True:
+        _, frame = cam.read()
+        cv2.imshow("Camera feed", frame)
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = holistic.process(image)
+        
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break 
 
 cam.release()
 cv2.destroyAllWindows()
