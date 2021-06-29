c08ff5eaa2c6a0c330e5039569f1bf03648a9660
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-06-30 03:16:27 +0430

    Now it's working fine

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ cam = cv2.VideoCapture(1)
 with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as holistic:
     while True:
         _, frame = cam.read()
-        cv2.imshow("Camera feed", frame)
+        
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         result = holistic.process(image)
-        
+         
+        mp_drawing.draw_landmarks(image, result.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, result.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, result.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+
+        cv2.imshow("Camera feed", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         if cv2.waitKey(1) & 0xff == ord('q'):
             break 
 
